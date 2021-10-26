aa28267176c84f3356fef141c5a0c8238742e33a
pierfrancescoscar <scarinci401@gmail.com>
2021-10-26 14:21:26 +0200

    add setup for "js mail" exercise

---
+++ mail/js/main.js
@@ -0,0 +1,16 @@
+// Mail
+// - crea una lista contenente almeno 3 indirizzi email
+// - chiedi all’utente la sua email,
+// - controlla che sia nella lista di chi può accedere (lista creata sopra al punto 1),
+// - stampa un messaggio appropriato sull’esito del controllo.
+
+// Ref
+const mailList = ['pierfrancesco@gmail.com', 'scarinci@gmail.com', 'pierfrancescoscarinci@gmail.com'];
+console.log(mailList);
+
+// Ask user's mail
+const askMail = prompt ('Qual è la tua e-mail?');
+console.log(askMail);
+
+// Check if the user's mail is in our list
+
