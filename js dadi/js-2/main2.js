70b22fd9df8ea8c891eb45a43998d5034ed900b7
pierfrancescoscar <scarinci401@gmail.com>
2021-10-26 15:51:49 +0200

    add alert with winner of the 2 generated numbers

--- js dadi/js-2/main2.js
+++ js dadi/js-2/main2.js
@@ -2,10 +2,24 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-// Ref 
 
 // Generating a random number from 1 to 6:
 
 const userNumber = Math.floor(Math.random() * 6) + 1;
 const cpuNumber = Math.floor(Math.random() * 6) + 1;
 console.log(userNumber);
+console.log(cpuNumber);
+
+// Let's show the winner: who has higher number wins
+
+    if (userNumber > cpuNumber) {
+        alert(`Il numero ${userNumber} è maggiore di ${cpuNumber} e quindi vince!`);
+    }
+    else {
+        alert(`Il numero ${cpuNumber} è maggiore di ${userNumber} e quindi vince!`)
+    }
+
+    
+
+
+
